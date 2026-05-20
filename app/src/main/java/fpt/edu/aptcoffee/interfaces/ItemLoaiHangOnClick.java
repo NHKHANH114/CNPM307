b5470d9f8be4748ff3c7634550426405248806ee
mlynie231 <hmelynie2301@gmail.com>
2026-05-21 01:41:40 +0700

    Me Ly-Them DAO,interface

---
+++ app/src/main/java/fpt/edu/aptcoffee/interfaces/ItemLoaiHangOnClick.java
@@ -0,0 +1,9 @@
+package fpt.edu.aptcoffee.interfaces;
+
+import android.view.View;
+
+import fpt.edu.aptcoffee.model.LoaiHang;
+
+public interface ItemLoaiHangOnClick {
+    void itemOclick(View view, LoaiHang loaiHang);
+}
---
+++ app/src/main/java/fpt/edu/aptcoffee/interfaces/ItemTangGiamSoLuongOnClick.java
@@ -0,0 +1,11 @@
+package fpt.edu.aptcoffee.interfaces;
+
+import android.view.View;
+
+import fpt.edu.aptcoffee.model.HangHoa;
+import fpt.edu.aptcoffee.model.HoaDonChiTiet;
+
+public interface ItemTangGiamSoLuongOnClick {
+    void itemOclick(View view, int indext, HoaDonChiTiet hoaDonChiTiet, HangHoa hangHoa);
+    void itemOclickDeleteHDCT(View view, HoaDonChiTiet hoaDonChiTiet);
+}
